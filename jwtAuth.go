0ad8be470db815b0168fddf29d5826fb7efc46d9
Supriyo <paulsupriyo64nk@gmail.com>
2023-10-07 20:54:10 +0530

    jwt authentication first stage

--- jwtAuth.go
+++ jwtAuth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/julienschmidt/httprouter"
@@ -11,10 +12,10 @@ import (
 // TODO: remove secret from here
 var secret = "MY_SECRET"
 
-func createJWT(userSignupRequest *UserSignupRequest) (string, error) {
+func createJWT(userLoginRequest *UserLoginRequest) (string, error) {
 	claims := &jwt.MapClaims{
-		"expiresAt": 15000,
-		"email":     userSignupRequest.Email,
+		"expiresAt": time.Now().Add(time.Hour * 24 * 30).Unix(),
+		"email":     userLoginRequest.Email,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -26,7 +27,7 @@ func validateJWT(tokenString string) (*jwt.Token, error) {
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
@@ -34,19 +35,27 @@ func validateJWT(tokenString string) (*jwt.Token, error) {
 	})
 }
 
-func withJWTAuth(handle httprouter.Handle, s *Storage) httprouter.Handle {
+func withJWTAuth(handle httprouter.Handle) httprouter.Handle {
 	return func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 		LogInfo().Println("Calling jwt middleware..")
 
-		tokenString := req.Header.Get("x-jwt-token")
-		token, err := validateJWT(tokenString)
+		cookie, err := req.Cookie("Auth")
 		if err != nil {
-			LogError().Println("Permisson denied!") // TODO: Redirect to the error page
-			return
+			// redirect to the login page
+			http.Redirect(res, req, "/login", http.StatusSeeOther)
+
+			if err == http.ErrNoCookie {
+				// If the cookie is not set, return an unauthorized status
+				res.WriteHeader(http.StatusUnauthorized)
+			}
+			// For any other type of error, return a bad request status
+			res.WriteHeader(http.StatusBadRequest)
 		}
-		if !token.Valid {
-			LogError().Println("Permisson denied!") // TODO: Redirect to the error page
-			return
+
+		token, err := validateJWT(cookie.Value)
+		if err != nil || !token.Valid {
+			// redirect to the login page
+			http.Redirect(res, req, "/login", http.StatusSeeOther)
 		}
 	}
 }
